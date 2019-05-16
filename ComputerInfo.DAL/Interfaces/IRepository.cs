11e1d8de5ace345541060c68392ab82ef0555f78
Dmytro Shevchenko <Dmytro_Shevchenko5@epam.com>
2019-05-16 17:37:26 +0300

    add repositories interface

---
+++ ComputerInfo.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using ComputerInfo.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerInfo.DAL.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : BaseEntity
+    {
+        void Create(T item);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Update(T item);
+        void Delete(int itemId);
+    }
+}
