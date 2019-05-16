5b801e1636c601bcec42b7851a8e268781f8bd43
Dmytro Shevchenko <Dmytro_Shevchenko5@epam.com>
2019-05-16 17:36:59 +0300

    add context

---
+++ ComputerInfo.DAL/EF/PCInfoContext.cs
@@ -0,0 +1,21 @@
+﻿using ComputerInfo.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerInfo.DAL.EF
+{
+    public class PCInfoContext : DbContext
+    {
+        public PCInfoContext(string connectionString)
+            :base(connectionString)
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PCInfoContext>());
+        }
+
+        public DbSet<PCInfo> PCInfos { get; set; }
+    }
+}
