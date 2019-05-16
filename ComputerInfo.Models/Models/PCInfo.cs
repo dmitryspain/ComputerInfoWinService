cd3564e0a6427bded88441628186852bbee90cbb
Dmytro Shevchenko <Dmytro_Shevchenko5@epam.com>
2019-05-16 17:38:40 +0300

    model added

---
+++ ComputerInfo.Models/Models/PCInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerInfo.Models.Models
+{
+    public class PCInfo : BaseEntity
+    {
+        public int CpuLoad { get; set; }
+        public int RamLoad { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public List<string> Users { get; set; }
+    }
+}
