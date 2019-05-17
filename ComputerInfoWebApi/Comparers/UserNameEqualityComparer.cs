9a291d1fc7d0ac5ad12cbc4c36809e5fcf7c1df3
Dmytro Shevchenko <dmytro_shevchenko5@epam.com>
2019-05-17 19:42:08 +0300

    user comparer added

---
+++ ComputerInfoWebApi/Comparers/UserNameEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿using ComputerInfo.Models.Models;
+using System.Collections.Generic;
+
+namespace ComputerInfoWebApi.Comparers
+{
+    public class UserNameEqualityComparer : IEqualityComparer<User>
+    {
+        public int GetHashCode(User obj)
+        {
+            return (obj == null) ? 0 : obj.Name.GetHashCode();
+        }
+
+        public bool Equals(User x, User y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x == null || y == null) return false;
+            return x.Name == y.Name;
+        }
+    }
+}
