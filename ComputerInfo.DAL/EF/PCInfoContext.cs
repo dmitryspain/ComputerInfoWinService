b8c724a33bf07fad59fba058301aebb8853f24c5
Dmytro Shevchenko <dmytro_shevchenko5@epam.com>
2019-05-17 19:40:32 +0300

    unused directives removed

--- ComputerInfo.DAL/EF/PCInfoContext.cs
+++ ComputerInfo.DAL/EF/PCInfoContext.cs
@@ -1,10 +1,5 @@
 ﻿using ComputerInfo.Models.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ComputerInfo.DAL.EF
 {
